5941124ec7de2c46c9c2c902ac213cb4f963374a
sharibarboza <barbozashari@gmail.com>
2018-01-24 13:28:54 -0700

    Add About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,33 @@ export default class About extends React.Component {
   
   render() {
     return (
-      <div></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <h2>About</h2>
+          </div>
+          <div className="col-lg-12">
+            <p>
+              <h4>Michelle</h4>
+              <ul>
+                <li>Computing Science at University of Alberta</li>
+                <li>Graduating: Spring 2018</li>
+                <li>BADASS</li>
+                <li>Hometown: Grande Prairie</li>
+              </ul>
+
+              <h4>Shari</h4>
+              <ul>
+                <li>Computing Science at University of Alberta</li>
+                <li>Graduating: Spring 2018</li>
+                <li>SO TRAGIC</li>
+                <li>Hometown: Edmonton</li>
+              </ul>
+
+            </p>
+          </div>
+        </div> 
+      </div>
     );
   }
   
